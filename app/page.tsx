9238c191b793e11e2e800419801ea2be964899d3
Marko Davkovski <marko.davkovski@gmail.com>
2024-07-06 13:40:35 +0200

    Register page initialization , register form, form validation and img data creation.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,37 @@
-import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-white text-3xl underline">Home</h1>
-      <Button>Click</Button>
+    <main className="flex h-screen max-h-screen">
+      {/* TODO: OTP verification | PassKeyModal*/}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"></Image>
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="flex justify-items-end text-dark-600 xl:text-left">
+              ©2024 CareSphere
+            </p>
+            <Link href="/?admin=true" className="text-blue-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </main>
   );
 }
