a8487dfe8ed682ec088c55c5250cc6833f67294f
Marko Davkovski <marko.davkovski@gmail.com>
2024-07-07 15:08:58 +0200

    Define API call actions.

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,8 +1,18 @@
 "use server";
 
 import { ID, Query } from "node-appwrite";
-import { users } from "../appwrite.config";
+import {
+  PROJECT_ID,
+  BUCKET_ID,
+  DATABASE_ID,
+  databases,
+  ENDPOINT,
+  PATIENT_COLLECTION_ID,
+  storage,
+  users,
+} from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { InputFile } from "node-appwrite/file";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -19,5 +29,46 @@ export const createUser = async (user: CreateUserParams) => {
       const documents = await users.list([Query.equal("email", [user.email])]);
       return documents?.users[0];
     }
+  }
+};
+
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId);
+    return parseStringify(user);
+  } catch (error: any) {
+    if (error && error?.code === 404) {
+      return null;
+    }
+    console.error(error);
+  }
+};
+
+export const registerPatient = async ({
+  identificationDocument,
+  ...patient
+}: RegisterUserParams) => {
+  try {
+    let file;
+    if (identificationDocument) {
+      const inputFile = InputFile.fromBuffer(
+        identificationDocument?.get("blobFile") as Blob,
+        identificationDocument?.get("filename") as string
+      );
+      file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
+    }
+    const newPatient = await databases.createDocument(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      ID.unique(),
+      {
+        identificationDocumentID: file?.$id || null,
+        identificationDocumentURL: `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
+        ...patient,
+      }
+    );
+    return parseStringify(newPatient);
+  } catch (error) {
+    console.error(error);
   }
 };
