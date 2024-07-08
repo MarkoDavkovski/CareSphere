242ded1db53dc360f4ee91bb46bcbe5b5826e771
Marko Davkovski <marko.davkovski@gmail.com>
2024-07-08 20:37:36 +0200

    Creation of get patient by id action

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -44,6 +44,22 @@ export const getUser = async (userId: string) => {
   }
 };
 
+export const getPatientById = async (userId: string) => {
+  try {
+    const patients = await databases.listDocuments(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      [Query.equal("userId", [userId])]
+    );
+    return parseStringify(patients?.documents[0]);
+  } catch (error: any) {
+    if (error && error?.code === 404) {
+      return null;
+    }
+    console.error(error);
+  }
+};
+
 export const registerPatient = async ({
   identificationDocument,
   ...patient
