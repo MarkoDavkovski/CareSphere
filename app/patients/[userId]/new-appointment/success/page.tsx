e2f2f8bd2f36297477c8a3e4901f80fa7e0c1546
Marko Davkovski <marko.davkovski@gmail.com>
2024-07-09 20:32:51 +0200

    Creation of the Success page

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -1,7 +1,78 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Doctors } from "@/constants";
+import { getAppointment } from "@/lib/actions/appointment.actions";
+import { formatDateTime } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 
-const Success = () => {
-  return <div>Success</div>;
+const Success = async ({
+  params: { userId },
+  searchParams,
+}: SearchParamProps) => {
+  const appointmentId = (searchParams?.appointmentId as string) || "";
+  const appointmentData = await getAppointment(appointmentId);
+  const appointment = appointmentData.documents[0];
+
+  const doctor = Doctors.find(
+    (doctor) => doctor.name === appointment.primaryPhysician
+  );
+
+  return (
+    <main className="flex h-screen max-h-screen px-[5%]">
+      <div className="success-img">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="logo"
+            className="h-10 w-fit"
+          />
+        </Link>
+        <section className="flex flex-col items-center">
+          <Image
+            src="/assets/gifs/success.gif"
+            height={300}
+            width={280}
+            alt="success"
+          />
+          <h2 className="header mb-6 max-w-[600px] text-center">
+            Your <span className="text-blue-500">appointment request</span> has
+            been successfully submitted!
+          </h2>
+          <p>We will be in touch shortly to confirm.</p>
+        </section>
+        <section className="request-details">
+          <p>Requested appointment details</p>
+          <div className="flex items-center gap-3">
+            <Image
+              src={doctor?.image!}
+              height={100}
+              width={100}
+              alt="doctor"
+              className="size-7"
+            />
+            <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+          </div>
+          <div className="flex gap-2 ">
+            <Image
+              src="/assets/icons/calendar.svg"
+              alt="calendar"
+              height={24}
+              width={24}
+            />
+            <p>{formatDateTime(appointment.schedule).dateTime}</p>
+          </div>
+        </section>
+        <Button variant="outline" className="bg-green-700" asChild>
+          <Link href={`/patients/${userId}/new-appointment`}>
+            New Appointment
+          </Link>
+        </Button>
+        <p className="copyright">©2024 CareSphere</p>
+      </div>
+    </main>
+  );
 };
 
 export default Success;
